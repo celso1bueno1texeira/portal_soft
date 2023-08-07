bdeb2db661ff51f458ef31c50942564062f7db9d
Celso Bueno <cbtbueno@gmail.com>
2023-08-07 11:38:43 -0300

    ajuste

---
+++ portal/01-service/create-service.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+NAMESPACE=default
+DEPLOYMENT=portal
+
+if [ ! -z "$(kubectl get service --namespace=$NAMESPACE | grep $DEPLOYMENT)" ]
+then
+    echo = "o service já existe!!!"
+    exit
+else
+    kubectl apply -f ./k8s/api/service.yaml
+fi
